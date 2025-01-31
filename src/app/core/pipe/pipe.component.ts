9bf8fd0eeae427fa99f86fe634dbd2a047aa6d93
Magnus <bcict20070@ttu.edu.gh>
2025-01-31 15:53:20 +0000

    update pipe component template and add necessary imports for data binding

--- src/app/core/pipe/pipe.component.ts
+++ src/app/core/pipe/pipe.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-
+import { CurrencyPipe, DatePipe, TitleCasePipe } from '@angular/common';
 @Component({
   selector: 'app-pipe',
-  imports: [],
+  imports: [CurrencyPipe, DatePipe, TitleCasePipe],
   templateUrl: './pipe.component.html',
   styleUrl: './pipe.component.css'
 })
 export class PipeComponent {
-
+  company = 'Angular';
+  purchaseDate = new Date();
+  purchaseAmount = 1000;
 }
