39c71a0d6e1c48b64b9f764867807bc00cc375fa
Magnus <bcict20070@ttu.edu.gh>
2025-02-01 19:08:42 +0000

    wertyuiop[

--- src/app/core/pipe/pipe.component.ts
+++ src/app/core/pipe/pipe.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { CurrencyPipe, DatePipe, TitleCasePipe } from '@angular/common';
+import {
+  CurrencyPipe, DatePipe, TitleCasePipe,
+  UpperCasePipe
+} from '@angular/common';
 @Component({
   selector: 'app-pipe',
-  imports: [CurrencyPipe, DatePipe, TitleCasePipe],
+  imports: [CurrencyPipe, DatePipe, TitleCasePipe, UpperCasePipe],
   templateUrl: './pipe.component.html',
   styleUrl: './pipe.component.css',
 })
 export class PipeComponent {
-  company = 'Angular';
+  company = 'acme corporation';
   purchaseDate = new Date();
   purchaseAmount = 1000;
+  firstName = 'john';
+  lastName = 'doe';
+  isAdmin = false;
 }
