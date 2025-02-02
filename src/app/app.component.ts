b8605f4f95013d0fd172151329658ddfe14e1f99
Magnus <bcict20070@ttu.edu.gh>
2025-02-02 13:09:11 +0000

    remove PipeComponent and its associated files; add Mycustompipe implementation and tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PipeComponent } from './core/pipe/pipe.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PipeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
