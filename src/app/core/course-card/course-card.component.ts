47c45d1f07635485e89a5389222596f70b3901b1
Magnus <bcict20070@ttu.edu.gh>
2025-02-04 15:02:53 +0000

    feat: update CourseCardComponent to display course details and integrate with DecoratorComponent

--- src/app/core/course-card/course-card.component.ts
+++ src/app/core/course-card/course-card.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { COURSES } from '../model/db.data';
+
 
 @Component({
   selector: 'app-course-card',
-  imports: [],
+  imports: [ ],
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.css'
 })
 export class CourseCardComponent {
 
+  coreCourse = COURSES[0];
+
+  rxjxCourse = COURSES[1];
+
+  ngrxCourse = COURSES[2];
 }
--- src/app/core/decorator/decorator.component.ts
+++ src/app/core/decorator/decorator.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { COURSES } from '../model/db.data';
+import { CourseCardComponent } from '../course-card/course-card.component';
 
 @Component({
   selector: 'app-decorator',
-  imports: [],
+  imports: [ CourseCardComponent],
   templateUrl: './decorator.component.html',
   styleUrl: './decorator.component.css'
 })
