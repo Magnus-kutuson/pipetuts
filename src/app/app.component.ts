cadcaa59a67f0e5eb973519d5bc0c4a2af1c9ccf
Magnus <bcict20070@ttu.edu.gh>
2025-02-04 14:04:35 +0000

    feat: include DecoratorComponent in app component and comment out FormsComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TransformComponent } from './transform/transform.component';
 import { FormsComponent } from './forms/forms.component';
+import { DecoratorComponent } from './core/decorator/decorator.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TransformComponent, FormsComponent],
+  imports: [RouterOutlet, TransformComponent, FormsComponent, DecoratorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
